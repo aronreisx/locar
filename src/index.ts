d209fae9c3dabbf78ffaee2a8f64a7ee1b51f7fa
Aron Reis <aronreis2@gmail.com>
2022-04-25 20:28:08 +0100

    ✨ Introduce server listening

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
