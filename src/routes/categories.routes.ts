4a841cf5529cfc77c7dba667876b8ba39ee3ec3b
Aron Reis <aronreis2@gmail.com>
2022-04-25 23:31:36 +0100

    ✨ Add Categories route Post

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,6 +5,20 @@ import { CategoryRepository } from '../repositories/CategoriesRepository';
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoryRepository();
 
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (!categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists' });
+  }
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
 categoriesRoutes.get('/', (request, response) => {
   const all = categoriesRepository.list();
 
