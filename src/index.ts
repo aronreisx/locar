b2bc405620e9944bc63492ef1c349062df639d28
Aron Reis <aronreis2@gmail.com>
2022-04-25 23:32:22 +0100

    ✨ Add Categories route endpoint middlaware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 const port = 3333;
 
