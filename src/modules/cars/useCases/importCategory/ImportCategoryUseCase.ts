f9861aaef076993ad6d1635d9d2c0a87826ff274
aronreisx <aronreis2@gmail.com>
2022-05-04 09:46:14 +0100

    🚧 Start  ImportCategory use case implementation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,19 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parsedFile = parse();
+
+    stream.pipe(parsedFile);
+
+    parsedFile.on('data', async (line) => {
+      console.time('line');
+      console.log(line);
+      console.timeEnd('line');
+    })
   }
 }
 
