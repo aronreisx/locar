48b19b869b84ee7abe7c5c14a8d0e8707af843bf
Aron Reis <aronreis2@gmail.com>
2022-05-11 20:34:10 +0100

    🔥 Delete TypeORM connection file

--- src/database/index.ts
+++
@@ -1,7 +0,0 @@
-import { DataSource } from "typeorm";
-
-import ormconfig from "../config/ormconfig";
-
-const dataSource: DataSource = new DataSource(ormconfig);
-
-const connection = await dataSource.initialize();
