7f02df5cc7a095fdc7f4de3f076e8231598888ab
aronreisx <aronreis2@gmail.com>
2022-05-21 18:38:57 +0100

    ✏️ Fix Prisma schema typos

--- prisma/migrations/20220520194435_create_category/migration.sql
+++ prisma/migrations/20220520194435_create_category/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "categories" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
 );
