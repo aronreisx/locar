9ad82808aaa1b6c9be4015ed8885ef56cf0b9524
aronreisx <aronreis2@gmail.com>
2022-07-18 21:30:30 +0100

    ✨ Use ensureAdmin on Categories & Specifications

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
+import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 import { ensureAuthentication } from '@shared/infra/http/middlewares/ensureAuthentication';
 
-import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
-import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
-import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
-
-const categoriesRoutes = Router();
+export const categoriesRoutes = Router();
 
 categoriesRoutes.use(ensureAuthentication);
 
@@ -16,12 +16,21 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post(
+  '/',
+  ensureAuthentication,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 const listCategoriesController = new ListCategoriesController();
 categoriesRoutes.get('/', listCategoriesController.handle);
 
 const importCategoryController = new ImportCategoryController();
-categoriesRoutes.post('/import', importCategoryController.handle);
-
-export { categoriesRoutes };
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  ensureAuthentication,
+  ensureAdmin,
+  importCategoryController.handle
+);
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 
+import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/createSpecificationController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 import { ensureAuthentication } from '@shared/infra/http/middlewares/ensureAuthentication';
 
-import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/createSpecificationController';
-
-const specificationsRoutes = Router();
+export const specificationsRoutes = Router();
 
 specificationsRoutes.use(ensureAuthentication);
 
 const createSpecificationController = new CreateSpecificationController();
-specificationsRoutes.post('/', createSpecificationController.handle);
-
-export { specificationsRoutes };
+specificationsRoutes.post(
+  '/',
+  ensureAuthentication,
+  ensureAdmin,
+  createSpecificationController.handle
+);
