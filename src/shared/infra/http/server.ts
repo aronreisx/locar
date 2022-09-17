a0fa5f3d9bd9cee93844f8a1bad5572be5c6e81d
aronreisx <aronreis2@gmail.com>
2022-09-17 15:53:28 +0100

    🚚 Separate App logic into dedicated file

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,27 +1,4 @@
-import dotenv from 'dotenv';
-import dotenvExpander from 'dotenv-expand';
-
-import 'reflect-metadata';
-import '@shared/container';
-
-import express from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-
-import { swaggerDoc } from '@config/swagger';
-import { errorHandler } from '@shared/infra/http/middlewares/errorHandler';
-import { router } from '@shared/infra/http/routes';
-
-dotenvExpander.expand(dotenv.config());
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-app.use(router);
-app.use(errorHandler);
+import app from './app';
 
 const { SERVER_PORT } = process.env;
 
