5d3076e2e9a02b591071c37f347a9692f829be77
aronreisx <aronreis2@gmail.com>
2022-09-17 16:19:00 +0100

    ♻️ Change Seed method

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -5,21 +5,20 @@ import { Categories } from './category';
 import { Specifications } from './specifications';
 import { Users } from './users';
 
-const prisma = new PrismaClient();
-
-async function Seed(prisma: PrismaClient): Promise<void> {
-  await Categories(prisma);
-  await Specifications(prisma);
-  await Users(prisma);
-  await Cars(prisma);
+async function Seed(databaseClient: PrismaClient): Promise<void> {
+  try {
+    await Categories(databaseClient);
+    await Specifications(databaseClient);
+    await Users(databaseClient);
+    await Cars(databaseClient);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  } finally {
+    await databaseClient.$disconnect();
+  }
 }
 
-Seed(prisma)
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (error) => {
-    console.error(error);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+const clientConnection = new PrismaClient();
+
+Seed(clientConnection);
