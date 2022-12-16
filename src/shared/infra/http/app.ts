afdf5c588d839284e925ff1f29d8a1e04913232d
aronreisx <aronreis2@gmail.com>
2022-12-16 13:18:54 +0000

    ♻️ Change Upload Config properties

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import { swaggerDoc } from '@config/swagger';
+import upload from '@config/upload';
 import { errorHandler } from '@shared/infra/http/middlewares/errorHandler';
 import { router } from '@shared/infra/http/routes';
 
@@ -17,6 +18,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 app.use(errorHandler);
