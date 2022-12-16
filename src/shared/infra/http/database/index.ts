a13f64e310b164fe9b9834cf771442b27f548c53
aronreisx <aronreis2@gmail.com>
2022-12-16 13:01:21 +0000

    💡 Add Prisma debugging parameters

--- src/shared/infra/http/database/index.ts
+++ src/shared/infra/http/database/index.ts
@@ -2,3 +2,23 @@ import { PrismaClient } from '@prisma/client';
 
 export type IDatabaseClient = PrismaClient;
 export const databaseClient = new PrismaClient();
+// {
+//   log: [
+//     {
+//       emit: 'stdout',
+//       level: 'query',
+//     },
+//     {
+//       emit: 'stdout',
+//       level: 'error',
+//     },
+//     {
+//       emit: 'stdout',
+//       level: 'info',
+//     },
+//     {
+//       emit: 'stdout',
+//       level: 'warn',
+//     },
+//   ],
+// }
