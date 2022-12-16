4585657d15c4fe8ca3addc45af0bda9647218660
aronreisx <aronreis2@gmail.com>
2022-12-16 13:02:12 +0000

    ✨ Add new date methods

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -4,4 +4,7 @@ export interface IDateProvider {
   dateTomorrow(): Date;
   dateNow(): Date;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
+  addHours(hours: number): Date;
+  compareIfDatesBefore(start_date: Date, end_date: Date): boolean;
 }
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -29,4 +29,15 @@ export class DayjsDateProvider implements IDateProvider {
     const start_date_utc = this.convertToUTC(start_date);
     return dayjs(end_date_utc).diff(start_date_utc, 'days');
   }
+  addDays(days: number): Date {
+    return dayjs().add(days, 'days').toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, 'hour').toDate();
+  }
+
+  compareIfDatesBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
 }
