33524215f1445820b2a2a3cf48be044d6a5d54a6
Aron Reis <aronreis2@gmail.com>
2022-12-17 20:41:48 +0000

    ✨ Use Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,7 @@ import { swaggerDoc } from '@config/swagger';
 import upload from '@config/upload';
 import { errorHandler } from '@shared/infra/http/middlewares/errorHandler';
 import { router } from '@shared/infra/http/routes';
+import cors from 'cors';
 
 dotenvExpander.expand(dotenv.config());
 
@@ -22,6 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 app.use(errorHandler);
 
